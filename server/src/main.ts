0a26b7b0e0e71f8d704b423159cc9fa9d907950f
amir-hosein-003 <ah.kanani003@gmail.com>
2025-11-03 19:51:12 +0330

    setGlobalPrefix in server (main.ts)

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
+  app.setGlobalPrefix('/api/v1');
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true,
