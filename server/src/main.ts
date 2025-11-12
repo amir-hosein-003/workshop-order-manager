015623b39e2b24b423023cce6b73a33c9ea16e6d
amir-hosein-003 <ah.kanani003@gmail.com>
2025-11-12 10:56:36 +0330

    add swagger-ui setup

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Workshop Order Manager')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    jsonDocumentUrl: 'swagger/json',
+  }); //  ==>  http://localhost:3000/api-json
 
   app.use(cookieParser());
   app.setGlobalPrefix('/api/v1');
